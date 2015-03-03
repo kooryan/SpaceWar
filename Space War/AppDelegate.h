40edc96b8b71322266170568c643b3e0abef40e5
Ryan Koo <ryankoo@Ryans-MacBook-Air.local>
2015-03-02 18:30:03 -0600

    Initial Commit

---
+++ Space War/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Space War
+//
+//  Created by Ryan Koo on 3/2/15.
+//  Copyright (c) 2015 Ryan Koo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Space War/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Space War
+//
+//  Created by Ryan Koo on 3/2/15.
+//  Copyright (c) 2015 Ryan Koo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
